5b3dfcce34d72e634bb78244aa7e7a253adc5c44
Silvia <silvia.rodriguez.alcaraz@rai.usc.es>
2019-07-28 23:49:59 +0200

    Updates

---
+++ precache-manifest.644f5f7cc6df3d83802791946422b600.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f63794eb5a3b809c1ca0",
+    "url": "/personal-portfolio/static/js/runtime~main.f63794eb.js"
+  },
+  {
+    "revision": "71fcdcb072feec040546",
+    "url": "/personal-portfolio/static/js/main.71fcdcb0.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/personal-portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "71fcdcb072feec040546",
+    "url": "/personal-portfolio/static/css/main.f814d691.chunk.css"
+  },
+  {
+    "revision": "fac6297489d56845b2b18c654dfb55d1",
+    "url": "/personal-portfolio/index.html"
+  }
+];
