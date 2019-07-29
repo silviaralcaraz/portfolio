ca3a43dcfa6828d2f1ddf672f2a7a4518a563ed2
Silvia <silvia.rodriguez.alcaraz@rai.usc.es>
2019-07-29 19:23:32 +0200

    Changes in organizations section

---
+++ build/precache-manifest.1db53b0151f041827d1998000e89ed01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0fad4c0f10d57b530fe4",
+    "url": "/portfolio/static/js/runtime~main.0fad4c0f.js"
+  },
+  {
+    "revision": "c938a6fb79a94eb8ec50",
+    "url": "/portfolio/static/js/main.c938a6fb.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/portfolio/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "c938a6fb79a94eb8ec50",
+    "url": "/portfolio/static/css/main.534c2e21.chunk.css"
+  },
+  {
+    "revision": "a67e21e34edf2cd05ca4e25d95b7cf93",
+    "url": "/portfolio/index.html"
+  }
+];
