b279f578ce0be46c60007656c20949db15b63574
Silvia <silvia.rodriguez.alcaraz@rai.usc.es>
2019-11-12 18:15:00 +0100

    aboutme updated

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -13,11 +13,12 @@ export default class About extends Component {
                         <div className="about-desc">
                         <span className="heading-meta">Sobre mí</span>
                         <h2 className="colorlib-heading">¿Quién soy?</h2>
-                        <p>Me llamo Silvia y soy una ingeniera informática de Galicia <i className="icon-umbrella" aria-hidden="true"/> (España)
-    					graduada por la Universidad de Santiago de Compostela.</p>
-    					<p>Realicé como Trabajo de Fin de Grado (TFG) una aplicación dentro del ámbito de la biotecnología donde se empleaban
-    					herramientas Big Data para mejorar su rendimiento. Actualmente,
-                        mis intereses profesionales siguen esa línea aunque también incluyen otros.</p>
+                        <p align="justify">Me llamo Silvia y soy una ingeniera informática de Galicia <i className="icon-umbrella" aria-hidden="true"/> graduada por la Universidad de Santiago de Compostela.
+                        En la actualidad, estudio el Máster Interuniversitario en Computación de Altas Prestaciones (HPC) ofertado por la Universidad de Santiago, Coruña y
+                        el Centro de Supercomputación de Galicia (CESGA).</p>
+    					<p align="justify">Como Trabajo de Fin de Grado (TFG) realicé una aplicación dentro del ámbito de la biotecnología donde se empleaban
+    					herramientas Big Data para mejorar su rendimiento. Este proyecto me permitió introducirme en el mundo de los
+                        datos masivos y, de hecho, hoy por hoy mis intereses profesionales siguen esa línea aunque incluyen otros:</p>
                     </div>
                     <h4>Intereses profesionales</h4>
                     <div class="row">
@@ -29,7 +30,7 @@ export default class About extends Component {
     					</div>
     					<div class="col-md-3 animate-box" data-animate-effect="fadeInTop">
     						<div class="services color-3">
-    							<span class="icon2"><i class="icon-data"></i></span>
+    							<span class="icon2"><i class="icon-code-outline"></i></span>
     							<h3>Big Data</h3>
     						</div>
     					</div>
@@ -41,39 +42,13 @@ export default class About extends Component {
     					</div>
                         <div class="col-md-3 animate-box" data-animate-effect="fadeInRight">
     						<div class="services color-2">
-    							<span class="icon2"><i class="icon-code-outline"></i></span>
-    							<h3>IA</h3>
+    							<span class="icon2"><i class="icon-cloud4"></i></span>
+    							<h3>Cloud Computing</h3>
     						</div>
     					</div>
     				</div>
 
-                    <h4>Otros intereses</h4>
-                    <div class="row">
-    					<div class="col-md-3 animate-box" data-animate-effect="fadeInLeft">
-    						<div class="services color-4">
-    							<span class="icon2"><i class="icon-music2"></i></span>
-    							<h3>Música</h3>
-    						</div>
-    					</div>
-    					<div class="col-md-3 animate-box" data-animate-effect="fadeInTop">
-    						<div class="services color-2">
-    							<span class="icon2"><i class="icon-book2"></i></span>
-    							<h3>Lectura</h3>
-    						</div>
-    					</div>
-    					<div class="col-md-3 animate-box" data-animate-effect="fadeInRight">
-    						<div class="services color-3">
-    							<span class="icon2"><i class="icon-camera3"></i></span>
-    							<h3>Fotografía</h3>
-    						</div>
-    					</div>
-    					<div class="col-md-3 animate-box" data-animate-effect="fadeInBottom">
-    						<div class="services color-1">
-    							<span class="icon2"><i class="icon-plane"></i></span>
-    							<h3>Viajes</h3>
-    						</div>
-    					</div>
-    				</div>
+
 
 
             </div>
