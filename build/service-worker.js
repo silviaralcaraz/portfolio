c113f15c83ae43505a5b083d1fd8c5140179273e
Silvia <silvia.rodriguez.alcaraz@rai.usc.es>
2019-11-12 22:05:54 +0100

    npm run deploy

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.09b7566a95cd055d3eb8d7ae7f37530c.js"
+  "/portfolio/precache-manifest.47800ac8f16cbf50f44a7f8bf30bbb66.js"
 );
 
 workbox.clientsClaim();
